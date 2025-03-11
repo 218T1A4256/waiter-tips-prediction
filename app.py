eb7c79e32ea4c15e6565d05a91607d9d4f2728a3
Rajulapati Sai Vamsi Krishna <142872706+218T1A4256@users.noreply.github.com>
2025-03-11 22:24:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import pickle
 
+app = Flask(__name__)
+
+# Load your model
+model = pickle.load(open('your_model.pkl', 'rb'))
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Waiter Tips Prediction Model is Running!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    # Process input and predict
+    prediction = model.predict([[data['total_bill'], data['size']]])
+    return jsonify({'prediction': prediction[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
